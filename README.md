00eb14f4dd445504a57e3e97985964b6838b1feb
Adrian Hesketh <adrianhesketh@hushmail.com>
2025-05-14 14:13:13 +0100

    chore: update the set command to be an upsert

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ version set
 ```
 
 ```
-Updated .version to version 0.0.2
+0.0.2
 ```
 
 ### push
--- set.go
+++ set.go
@@ -15,13 +15,11 @@ func (c SetCommand) Run(ctx context.Context) (err error) {
 	if err != nil {
 		return err
 	}
-	if updated == current && current != "" {
-		fmt.Printf("No change, current version is %q\n", current)
-		return nil
+	if updated != current {
+		if err := os.WriteFile(c.DefaultArgs.Filename, []byte(updated), 0644); err != nil {
+			return err
+		}
 	}
-	if err := os.WriteFile(c.DefaultArgs.Filename, []byte(updated), 0644); err != nil {
-		return err
-	}
-	fmt.Printf("Updated %q from %q to %q\n", c.DefaultArgs.Filename, current, updated)
+	fmt.Print(updated)
 	return nil
 }
